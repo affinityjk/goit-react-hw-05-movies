b2d531d6abe50460e55f3363366a835107f54a47
Julia Kutsyn <yuliyaflm@gmail.com>
2021-08-23 19:03:20 +0300

    adds dependencies and pages

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# goit-react-hw-05-movies
+
+React-hw-05-movies
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+// import { Switch, Router } from "react-router-dom";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
